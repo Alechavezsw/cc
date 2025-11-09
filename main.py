cd9855c550591920cd8405fbdf00c87b8487f6d0
Alechavezsw <sarmientowebb@gmail.com>
2025-11-09 10:43:19 -0300

    Configurar Flask para servir HTML y habilitar CORS

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app)  # Permite que el HTML acceda a la API desde cualquier origen
+
+@app.route('/')
+def index():
+    html_file = '5f822cb6-316e-49b9-9a1b-70ec38c7942f.htm'
+    if os.path.exists(html_file):
+        return send_file(html_file)
+    return "HTML file not found", 404
 
 @app.route('/languages', methods=['GET'])
 def get_languages():
@@ -14,5 +24,5 @@ def get_languages():
     return jsonify(languages)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
